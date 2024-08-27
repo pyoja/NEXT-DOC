4daa9c999565d989923bd5469b5a918152661b45
pyoja <wjdgns13nr@naver.com>
2024-08-27 10:33:35 +0900

    usePathname

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { NavLinks } from "@/app/ui/nav-links";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <NavLinks />
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
