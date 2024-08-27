b23a8f98dd0d882e6963a44161e43adc326edfd5
pyoja <wjdgns13nr@naver.com>
2024-08-27 10:39:43 +0900

    React Fragment (<>...</>)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "NEXT.JS 공식문서 따라하기",
 };
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <h2>
+        <Link href="/dashboard">Dashboard</Link>
+      </h2>
+    </>
+  );
 }
