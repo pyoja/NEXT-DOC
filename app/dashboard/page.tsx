5e9a4dcb701f0ff7da00fb3c2e7098b234f34f01
pyoja <wjdgns13nr@naver.com>
2024-08-27 10:29:24 +0900

    metadata

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "여기는 대시보드",
+};
+
 export default function DashboardPage() {
   return <h1>hello dashboard</h1>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "NEXT.JS 공식문서 따라하기",
+};
+
 export default function Page() {
   return <h1>Hello, Next.js!</h1>;
 }
